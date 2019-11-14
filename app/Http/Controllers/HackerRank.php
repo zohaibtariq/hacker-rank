c00feb5a66c33d63ebf3c3c104a86f0fce2e9cde
Zohaib Tariq <zohaib.tariq@arpatech.com>
2019-11-14 12:26:29 +0500

    made modification to note timings and limit wins for test cases.

--- app/Http/Controllers/HackerRank.php
+++ app/Http/Controllers/HackerRank.php
@@ -6,6 +6,13 @@ use Illuminate\Http\Request;
 
 class HackerRank extends Controller
 {
+
+    public function __construct()
+    {
+        $this->script_time_end = 0;
+        $this->script_time_start = 0;
+    }
+
     /**
      * Handle the incoming request.
      *
@@ -14,6 +21,8 @@ class HackerRank extends Controller
      */
     public function __invoke(Request $request)
     {
+        $this->script_time_start = microtime(true);
+        $this->data_time_start = microtime(true);
         $this->test($request->case);
     }
 
@@ -37,7 +46,11 @@ class HackerRank extends Controller
     }
 
     public function test($filename) {
-        $stdin = fopen(storage_path('hacker-rank\input' . $filename . '.txt'), "r");
+//        $a = [1,2,3,4,5,6,7,8,9,10,11,12];
+//        $a = array_splice($a, 0, 10);
+//        dd($a);
+        $root_files_path = 'hacker-rank'.config('main.DS');
+        $stdin = fopen(storage_path($root_files_path . 'input' . $filename . '.txt'), "r");
         fscanf($stdin, "%[^\n]", $nkq_temp);
         $nkq = explode(' ', $nkq_temp);
         $n = intval($nkq[0]);
@@ -54,26 +67,41 @@ class HackerRank extends Controller
             $queries[] = array_map('intval', preg_split('/ /', $queries_temp, -1, PREG_SPLIT_NO_EMPTY));
         }
         fclose($stdin);
-        $stdout = fopen(storage_path('hacker-rank\output' . $filename . '.txt'), "r");
+        $stdout = fopen(storage_path($root_files_path . 'output' . $filename . '.txt'), "r");
         $expected = [];
         while(!feof($stdout)){
             $expected[] = intval(fgets($stdout));
         }
-        $wins = $this->fightingPits($k, $fighters, $queries);
+        $this->data_time_end = microtime(true);
+//        dd([
+//            [$n, $q, $k],
+//            [count($fighters), count($queries), count($expected)]
+//        ]);
+        $resultDataSet = $this->fightingPits($k, $fighters, $queries);
+//        'script_time' => ($this->script_time_end - $this->script_time_start)/60 . ' Mins ::: ' . ($this->script_time_end - $this->script_time_start) . ' Seconds',
+//            'data_time' => ($this->data_time_end - $this->data_time_start)/60 . ' Mins ::: ' . ($this->data_time_end - $this->data_time_start) . ' Seconds',
+//            'wins' => $teamWins,
+//            'restricted_to' => $restrictWins,
+        $wins = $resultDataSet['wins'];
+        $restricted_to = intval($resultDataSet['restricted_to']);
+        if($restricted_to > 0){
+            $expected = array_splice($expected, 0, $restricted_to);
+        }
         if($expected === $wins){
-            dd('All Test Cases Passed');
+            dd('All Test Cases Passed', $resultDataSet['script_time'], $resultDataSet['data_time']);
         }else{
             $result = $this->check($expected, $wins);
             if(!$result){
-                dd('All Test Cases Passed');
+                dd('All Test Cases Passed', $resultDataSet['script_time'], $resultDataSet['data_time']);
             }else{
-                dd('Failed Test Cases', $result);
+                dd('Failed Test Cases', $resultDataSet['script_time'], $resultDataSet['data_time'], $result);
             }
         }
     }
 
     public function fightingPits($k, $fighters, $queries) {
         $debugQuery = -1; // 5
+        $restrictWins = 10;
         $teams = [];
         $teamWins = [];
         for ($team_init = 1; $team_init <= $k; $team_init++) {
@@ -85,6 +113,7 @@ class HackerRank extends Controller
         foreach($teams as $teamIndex => $team){
             rsort($teams[$teamIndex]);
         }
+        $this->script_time_end = microtime(true);
         foreach($queries as $queryIndex => $query) {
             if($query[0] === 1){
                 array_unshift($teams[$query[2]], $query[1]);
@@ -101,8 +130,10 @@ class HackerRank extends Controller
                     dump('Before Match');
                     dump('TeamX');
                     dump($teamX);
+                    dump($teams[$query[1]]);
                     dump('TeamY');
                     dump($teamY);
+                    dump($teams[$query[2]]);
                 }
                 while(count($teamX) > 0 && count($teamY) > 0 ){
                     if($queryIndex === $debugQuery){
@@ -110,23 +141,29 @@ class HackerRank extends Controller
                         dump('Before X fight');
                         dump('TeamX');
                         dump($teamX);
+                        dump($teams[$query[1]]);
                         dump('TeamY');
                         dump($teamY);
+                        dump($teams[$query[2]]);
                     }
                     array_splice($teamY, 0, $teamX[0]);
                     if($queryIndex === $debugQuery){
                         dump('After X fight');
                         dump('TeamX');
                         dump($teamX);
+                        dump($teams[$query[1]]);
                         dump('TeamY');
                         dump($teamY);
+                        dump($teams[$query[2]]);
                     }
                     if($queryIndex === $debugQuery){
                         dump('Before Y fight');
                         dump('TeamX');
                         dump($teamX);
+                        dump($teams[$query[1]]);
                         dump('TeamY');
                         dump($teamY);
+                        dump($teams[$query[2]]);
                     }
                     if(count($teamY) > 0){
                         if($queryIndex === $debugQuery){
@@ -138,8 +175,10 @@ class HackerRank extends Controller
                         dump('After Y fight');
                         dump('TeamX');
                         dump($teamX);
+                        dump($teams[$query[1]]);
                         dump('TeamY');
                         dump($teamY);
+                        dump($teams[$query[2]]);
                         dump('while fight ends >>>');
                     }
                 }
@@ -147,8 +186,10 @@ class HackerRank extends Controller
                     dump('After Match');
                     dump('TeamX');
                     dump($teamX);
+                    dump($teams[$query[1]]);
                     dump('TeamY');
                     dump($teamY);
+                    dump($teams[$query[2]]);
                 }
                 if(count($teamX) > 0){
                     if($queryIndex === $debugQuery){
@@ -168,7 +209,16 @@ class HackerRank extends Controller
                     $teamWins[] = $query[2];
                 }
             }
+            if(count($teamWins) === $restrictWins){
+                break;
+            }
         }
-        return $teamWins;
+        $this->script_time_end = microtime(true);
+        return [
+            'script_time' => ($this->script_time_end - $this->script_time_start)/60 . ' Mins ::: ' . ($this->script_time_end - $this->script_time_start) . ' Seconds',
+            'data_time' => ($this->data_time_end - $this->data_time_start)/60 . ' Mins ::: ' . ($this->data_time_end - $this->data_time_start) . ' Seconds',
+            'wins' => $teamWins,
+            'restricted_to' => $restrictWins,
+        ];
     }
 }
---
+++ config/main.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'DS' => env('DIRECTORY_SEPARATOR', '/')
+];
