813c7b20cd66bf4f1113e69f73cb0c9f65403c34
zohaib tariq <se.zohaib@gmail.com>
2019-11-21 23:44:24 +0500

    done Morganised string...

--- app/Http/Controllers/Morgan.php
+++ app/Http/Controllers/Morgan.php
@@ -19,38 +19,64 @@ class Morgan extends Controller
     }
 
     public function morganAndString($a, $b){
-        $a = strtoupper($a);
-        $b = strtoupper($b);
-        $a_length = strlen($a);
-        $b_length = strlen($b);
-        $length = ($a_length > $b_length)?$a_length:$b_length;
-        $all = [];
+        $string = '';
         $debug = false;
-        for($i=0; ($i <= ($length + 1)); $i++){
-            if($debug)
-                dd($a[0], $b[0]);
-            if(!isset($a[0]) || !isset($b[0]))
-                break;
-            else if(ord($a[0]) < ord($b[0])){
-                $all[] = $a[0];
+        while(isset($a[0]) && isset($b[0])){
+            $ord_first_a = ord($a[0]);
+            $ord_first_b = ord($b[0]);
+            if($debug){
+                dump('while start');
+                dump($a[0] . ':' . $ord_first_a . '<>' . $b[0] . ':' . $ord_first_b);
+                dd('while end');
+            }
+            if($ord_first_a < $ord_first_b){
+                $string .= $a[0];
+                if($this->matched($string))
+                    $debug = true;
                 $a = substr($a, 1);
             }
-            else if(ord($a[0]) > ord($b[0])){
-                $all[] = $b[0];
+            else if($ord_first_a > $ord_first_b){
+                $string .= $b[0];
+                if($this->matched($string))
+                    $debug = true;
                 $b = substr($b, 1);
-            }else if(ord($a[0]) === ord($b[0])){
-                $all[] = $a[0];
-                $a = substr($a, 1);
-            }
-            if(trim(implode('', $all)) === 'FPGNSJXJVQBQGDUVFEPKVCVDBYUJBLZAEJPOVOXCKWAWYOMJCSQZ'){
-                $debug = true;
+            }else if($ord_first_a === $ord_first_b){
+                $ord_second_a = (isset($a[1]))?ord($a[1]):0;
+                $ord_second_b = (isset($b[1]))?ord($b[1]):0;
+                if(($ord_first_a + $ord_second_a) < ($ord_first_b + $ord_second_b)){
+                    $string .= $a[0];
+                    if($this->matched($string))
+                        $debug = true;
+                    $a = substr($a, 1);
+                }else{
+                    $string .= $b[0];
+                    if($this->matched($string))
+                        $debug = true;
+                    $b = substr($b, 1);
+                }
             }
         }
-        if($a !== '')
-            $all[] = $a;
-        if($b !== '')
-            $all[] = $b;
-        return trim(implode('', $all));
+        $debug2 = false;
+        $remaining_string = (($a !== '' && $b === '')?$a:(($b !== '' && $a === '')?$b:''));
+        $remaining_string_count = strlen($remaining_string);
+        for($i=0; $i < $remaining_string_count; $i++){
+            if($debug2){
+                dump('remaining_string');
+                dd($remaining_string);
+            }
+            $string .= $remaining_string[0];
+            if($this->matched($string))
+                $debug2 = true;
+            $remaining_string = substr($remaining_string, 1);
+        }
+        return $string;
+    }
+
+    public function matched($str = null){
+        if($str === 'KMNCCCGLIQXPRCEWDGRXNXZAZKMJEATBKNOVATQJJXCMFDQUJKQWVJZDNOYSXYTRDPPHJBCBMCRVMCAKRKLCQYKAUEVQQKAUQEAUEWPDQGHNFBHPXFXAGFMWUXFONMVGVQRMZFWTSIMBGHSGDLCROMQXWKMJSYJQRYVLEUAIHLCQTYHXWYMVLAOKPZPLKUBMYKINQFFOOXAFNYYGJBQKMWCSUTNKTJUVOJRPVYNPPLPPRVVKIVGWJTMQGHWYMTDIUGRKHEULXMGAVVVJJOGKIWRFYGKXOTHXCIYGTTLMMZQIATWDPEKKFTQGQHRRRWIAUPSOVTHYTHLXSXTDJILFXFNYCCLBTSHZHEHOVQFWNYGSNHLBDRRBXWQTECGPPKIIWHGQLILSJWRUUDMTAMNCKTMRFPBBRVXHXPXZUHSPHFDGYYISOJBIQCHZRUICMEHOLKJPEEDXORUXFYFEWCUCMDPOEPADIJNYGLLHEYKBWQRTDLXONRVOLGHCGOZXLAVCQBXZPZLBCEOSSXIRWWAJVYOPZGUSLHKAFFJUNOXVAEJXEMWKVIPMQNDHYDVVQQTWMXPUPNRRXFTFRTNZFESTQBJCKOSIKMXJAFJVXUMZOEJHLKORMLFXLSKNMYMJKSKBVJRDHPVFCLDWYICYXQBZYCAIOQRCGMKTEKDAHBNBILZCUOYSJETBTESGGSUWQWJSMFJORCLRKYRPRWJBKABHUYODSUAITIBDCAQIFSWJUMHQRWNBTRTESXHUIJVNQXQAVUDWIUOFEHKIVGYMKLDBEBZYMEEFFGBEHDVLFGDYSKOSABLCTRVSTMSGAWCTKPKAAMWMXSEIKZBRTVRBQORSFMWHSCMAFRJUGCMNZMLBYLNBCAWOFQPALTPETUCVAMYCPAEBRTKQHZUFEKIYYYHGSEMQCDCMCXNUVRNBLHYVKVASWZQQRRGLFRPZJOWKPQYEILDBWHSIBMCSNYQUCSSEMMJJDITAPMSRPNJMLSYULSJAWXIHZWODBVUDEKOEHGBNLCHFZXGXAZWZCWOCDJVPFSUHASCXVYTNMPWSWBWHGOZDIFSXKSNVBFUOCBDQUIIXBXBGLEHBGIKZTCAXNFLLYZVWCXVDOJLJCBWOKLCLEFGOKQEFMYCWPCLLTUWNSPNDKGEWEQFRDZGOAVGUWLARKGKFLIIPICSVJAPJNDXMZAGWSVWGFABDHIPSUHHVXADPIMJYUAQWQWKPDOIDKGANHZYUOCOWMNMZNHZWQWAHYPPXGGWUKXGLXVFVBZZRZEXZVMYRMNXFWAFDDSZIFZXTPLAPKQODXJIAPHPGPIXUFHXSAFJHEAMEJRKLFFRNYRSAVTRQXCGIAPRJKFDYIPIREXAEAFRKKSOQUIVWSADJBXAXSZBTTOFPAWHMLGSEWUBKOSFYSPUHNZUFWBJYKRWPBAEOUYHMTXOBVCSXFPQTUFCCDYTECZJPQCGIVJGHHOOLAKTCXFCJNFEACQYLNFIQJQAPTNZWXYSFTMOYHUZCUODUFWGRTUJRVDUMDDNMKODMTJFMVOJPFNLXXNPLBSIFZGYJUBGRCKGOUVASOADPNNRUUXEZMFRPNPFOUFZSDJWKGMZLIXFHRKAIRSKIMLSWKFJGMOCEAGFBYLCWELWFZIAZOMOUBNXJVNMDOUBYIFKHXWFMBRDWHRRMQXNHGUSQEMLLWUQJLSXSBNVIHQDIYTTAYEKFFXOOJYOUZGTTOWXHYDDMFXRTADIMKMYBQACKHWUCAWADIRMXNQFPRJVYQJGHFESDAXOARREHNOXWRPRMUGAOIMMTCVHIRQPIQCZNRLLSIAMJNRZNKAWYCOCHEIWNSJRMQUVNNNHRIOCZZ')
+            return true;
+        else
+            return false;
     }
 
     public function test($request){
@@ -74,9 +100,14 @@ class Morgan extends Controller
             $expected[] = trim(preg_replace('/\s\s+/', '', fgets($stdout)));
         }
         fclose($stdout);
+        var_dump($strings[0]);
+        var_dump($expected[0]);
+        exit;
+        // dd(strlen($strings[0]), strlen($expected[0]), $strings[0] === $expected[0], $strings[0], $expected[0]);
+        // dd(count($expected) === count($strings), $expected === $strings);
         //dd(count($expected) === count($strings));
         //dd($expected === $strings);
-        dd($strings[1], $expected[1]);
+        //dd($strings[1], $expected[1]);
         dd(strlen($strings[0]), strlen($expected[0]));
         dd($strings[0] === $expected[0]);
         dd($strings[1] === $expected[1]);
