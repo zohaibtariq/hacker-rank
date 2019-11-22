95cf6d1643274468a017c4d3c6edb1cf944c1710
Zohaib Tariq <zohaib.tariq@arpatech.com>
2019-11-22 13:04:05 +0500

    grading students done

--- app/Http/Controllers/Test.php
+++ app/Http/Controllers/Test.php
@@ -15,11 +15,31 @@ class Test extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        $grades = [86,30,0,16,51,53,42,48,22,69,12,27,34,24,95,16,32,22,52,56,71,95];
+        $final_grades = [];
+        foreach($grades as $grade){
+            $grade = intval($grade);
+            $multiple_of = 5;
+            $next_multiple_is = intval((round($grade)%$multiple_of === 0) ? round($grade) : round(($grade+$multiple_of/2)/$multiple_of)*$multiple_of);
+            $next_multiple_of_5 = $next_multiple_is;
+            if($grade < 38)
+                $final_grades[] = $grade;
+            else if(($next_multiple_of_5 - $grade) < 3)
+                $final_grades[] = $next_multiple_of_5;
+            else if(($next_multiple_of_5 - $grade) === 3)
+                $final_grades[] = $grade;
+            else
+                $final_grades[] = $grade;
+        }
+        dd($grades, $final_grades === [86,30,0,16,51,55,42,50,22,70,12,27,34,24,95,16,32,22,52,56,71,95]);
+    }
+
+    function nextMultiple($number,$multiple_of=5) {
+        return (round($number)%$multiple_of === 0) ? round($number) : round(($number+$multiple_of/2)/$multiple_of)*$multiple_of;
     }
 
 }
-interface MyInterface {
+/*interface MyInterface {
 //    public function callMe();
     public function callMe();
 }
@@ -41,7 +61,7 @@ class MyNewClass extends AbstractClass {
     public function absMethod1(){
         return 'MyNewClass class abs method';
     }
-}
+}*/
 //$myNewClass = new MyNewClass();
 //dd($myNewClass->absMethod());
 /*class MyStaticClass {
@@ -78,8 +98,8 @@ var_dump($anotherObj === SingletonChild::getInstance());*/
     }
 }
 
-$x = Foo::baz();  // $x is now a `Foo`
-dd($x);
+$multiple_of = Foo::baz();  // $multiple_of is now a `Foo`
+dd($multiple_of);
 
 class Bar extends Foo
 {
@@ -87,7 +107,7 @@ class Bar extends Foo
 
 $z = Bar::baz();  // $z is now a `Foo`
 dd($z);*/
-class Foo
+/*class Foo
 {
     public static function baz() {
         return new static();
@@ -98,7 +118,7 @@ class Bar extends Foo
 {
 }
 $wow = Bar::baz();
-dd($wow); // $wow is now a `Bar`, even though `baz()` is in base `Foo`
+dd($wow); // $wow is now a `Bar`, even though `baz()` is in base `Foo`*/
 /*
  * shortly: new static() - returns the object of current class, regardless of which classes is extended, and new self() - returns the object from the class in which that method was declared or extended(last version of the function)
  * */
