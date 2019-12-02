289f665a0467adcb0fb62a52c03d1cd0c6a83e9e
Zohaib Tariq <zohaib.tariq@arpatech.com>
2019-12-02 16:57:23 +0500

    working on morgan string incomplete

--- app/Http/Controllers/Morgan.php
+++ app/Http/Controllers/Morgan.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 class Morgan extends Controller
 {
 
+    public function __construct()
+    {
+        $this->string = '';
+        $this->arr_string = [];
+    }
+
     /**
      * Handle the incoming request.
      *
@@ -18,62 +24,90 @@ class Morgan extends Controller
         $this->test($request);
     }
 
+    public function concat($string = '', $debug = false){
+        $this->arr_string[] = $string;
+        $this->string .= $string;
+        if($debug){
+            dd($string, $this->arr_string, $this->string);
+        }
+    }
+
     public function morganAndString($a, $b){
+        dd($a, $b);
         $string = '';
-        $debug = false;
         while(isset($a[0]) && isset($b[0])){
             $ord_first_a = ord($a[0]);
             $ord_first_b = ord($b[0]);
-            if($debug){
-                dump('while start');
-                dump($a[0] . ':' . $ord_first_a . '<>' . $b[0] . ':' . $ord_first_b);
-                dd('while end');
-            }
             if($ord_first_a < $ord_first_b){
                 $string .= $a[0];
-                if($this->matched($string))
-                    $debug = true;
                 $a = substr($a, 1);
             }
             else if($ord_first_a > $ord_first_b){
                 $string .= $b[0];
-                if($this->matched($string))
-                    $debug = true;
                 $b = substr($b, 1);
             }else if($ord_first_a === $ord_first_b){
-                $ord_second_a = (isset($a[1]))?ord($a[1]):0;
-                $ord_second_b = (isset($b[1]))?ord($b[1]):0;
-
-                if(($ord_first_a + $ord_second_a) < ($ord_first_b + $ord_second_b)){
-                    $string .= $a[0];
-                    if($this->matched($string))
-                        $debug = true;
-                    $a = substr($a, 1);
-                }else{
-                    $string .= $b[0];
-                    if($this->matched($string))
-                        $debug = true;
-                    $b = substr($b, 1);
+                $longer_length = (strlen($a) >= strlen($b))?strlen($a):strlen($b);
+                for($counter = 0; $counter < $longer_length; $counter++){
+                    if(!isset($a[$counter]) || !isset($b[$counter])){
+                        if($a == '' || $b == ''){
+                            break;
+                        }else{
+                            if(!isset($a[$counter])){
+                                $string .= $a[0];
+                                $a = substr($a, 1);
+                                break;
+                            }else if(!isset($b[$counter])){
+                                $string .= $b[0];
+                                $b = substr($b, 1);
+                                break;
+                            }else{
+                                die('Problem Occured');
+                            }
+                        }
+                    }
+                    else if(ord($a[$counter]) === ord($b[$counter])){
+                        continue;
+                    }else if(ord($a[$counter]) < ord($b[$counter])){
+                        $string .= $a[0];
+                        $a = substr($a, 1);
+                        break;
+                    }else if(ord($a[$counter]) > ord($b[$counter])){
+                        $string .= $b[0];
+                        $b = substr($b, 1);
+                        break;
+                    }
                 }
             }
         }
-        $debug2 = false;
         $remaining_string = (($a !== '' && $b === '')?$a:(($b !== '' && $a === '')?$b:''));
-        $remaining_string_count = strlen($remaining_string);
-        for($i=0; $i < $remaining_string_count; $i++){
-            if($debug2){
-                dump('remaining_string');
-                dd($remaining_string);
-            }
-            $string .= $remaining_string[0];
-            if($this->matched($string))
-                $debug2 = true;
-            $remaining_string = substr($remaining_string, 1);
-        }
+        $string .= $remaining_string;
         return $string;
     }
 
+    public function log($string1 = null, $string2 = null, $take_char = null, $modified_string1 = null, $modified_string2 = null, $final_string = ''){
+        return false;
+        dump('=== Logging start === ');
+        dump('string 1 before : ' . $string1);
+        dump('string 2 before : ' . $string2);
+        dump('take char : ' . $take_char . ' from ' . (($modified_string1 !== null)? 'string 1':'string 2'));
+        if($modified_string1 !== null)
+            dump('string 1 after : ' . $modified_string1);
+        if($modified_string2 !== null)
+            dump('string 2 after : ' . $modified_string2);
+        if($final_string !== '')
+            dump('final string becomes : ' . $final_string);
+        dump('=== Logging end === ');
+    }
+
+/*
+    public function update($string_num = 0, $concat = '', &$substr){
+        $this->concat($concat);
+        $substr = &substr($substr, 1);
+    }
+*/
+
     public function matched($str = null){
+        return false;
         if($str === 'KMNCCCGLIQXPRCEWDGRXNXZAZKMJEATBKNOVATQJJXCMFDQUJKQWVJZDNOYSXYTRDPPHJBCBMCRVMCAKRKLCQYKAUEVQQKAUQEAUEWPDQGHNFBHPXFXAGFMWUXFONMVGVQRMZFWTSIMBGHSGDLCROMQXWKMJSYJQRYVLEUAIHLCQTYHXWYMVLAOKPZPLKUBMYKINQFFOOXAFNYYGJBQKMWCSUTNKTJUVOJRPVYNPPLPPRVVKIVGWJTMQGHWYMTDIUGRKHEULXMGAVVVJJOGKIWRFYGKXOTHXCIYGTTLMMZQIATWDPEKKFTQGQHRRRWIAUPSOVTHYTHLXSXTDJILFXFNYCCLBTSHZHEHOVQFWNYGSNHLBDRRBXWQTECGPPKIIWHGQLILSJWRUUDMTAMNCKTMRFPBBRVXHXPXZUHSPHFDGYYISOJBIQCHZRUICMEHOLKJPEEDXORUXFYFEWCUCMDPOEPADIJNYGLLHEYKBWQRTDLXONRVOLGHCGOZXLAVCQBXZPZLBCEOSSXIRWWAJVYOPZGUSLHKAFFJUNOXVAEJXEMWKVIPMQNDHYDVVQQTWMXPUPNRRXFTFRTNZFESTQBJCKOSIKMXJAFJVXUMZOEJHLKORMLFXLSKNMYMJKSKBVJRDHPVFCLDWYICYXQBZYCAIOQRCGMKTEKDAHBNBILZCUOYSJETBTESGGSUWQWJSMFJORCLRKYRPRWJBKABHUYODSUAITIBDCAQIFSWJUMHQRWNBTRTESXHUIJVNQXQAVUDWIUOFEHKIVGYMKLDBEBZYMEEFFGBEHDVLFGDYSKOSABLCTRVSTMSGAWCTKPKAAMWMXSEIKZBRTVRBQORSFMWHSCMAFRJUGCMNZMLBYLNBCAWOFQPALTPETUCVAMYCPAEBRTKQHZUFEKIYYYHGSEMQCDCMCXNUVRNBLHYVKVASWZQQRRGLFRPZJOWKPQYEILDBWHSIBMCSNYQUCSSEMMJJDITAPMSRPNJMLSYULSJAWXIHZWODBVUDEKOEHGBNLCHFZXGXAZWZCWOCDJVPFSUHASCXVYTNMPWSWBWHGOZDIFSXKSNVBFUOCBDQUIIXBXBGLEHBGIKZTCAXNFLLYZVWCXVDOJLJCBWOKLCLEFGOKQEFMYCWPCLLTUWNSPNDKGEWEQFRDZGOAVGUWLARKGKFLIIPICSVJAPJNDXMZAGWSVWGFABDHIPSUHHVXADPIMJYUAQWQWKPDOIDKGANHZYUOCOWMNMZNHZWQWAHYPPXGGWUKXGLXVFVBZZRZEXZVMYRMNXFWAFDDSZIFZXTPLAPKQODXJIAPHPGPIXUFHXSAFJHEAMEJRKLFFRNYRSAVTRQXCGIAPRJKFDYIPIREXAEAFRKKSOQUIVWSADJBXAXSZBTTOFPAWHMLGSEWUBKOSFYSPUHNZUFWBJYKRWPBAEOUYHMTXOBVCSXFPQTUFCCDYTECZJPQCGIVJGHHOOLAKTCXFCJNFEACQYLNFIQJQAPTNZWXYSFTMOYHUZCUODUFWGRTUJRVDUMDDNMKODMTJFMVOJPFNLXXNPLBSIFZGYJUBGRCKGOUVASOADPNNRUUXEZMFRPNPFOUFZSDJWKGMZLIXFHRKAIRSKIMLSWKFJGMOCEAGFBYLCWELWFZIAZOMOUBNXJVNMDOUBYIFKHXWFMBRDWHRRMQXNHGUSQEMLLWUQJLSXSBNVIHQDIYTTAYEKFFXOOJYOUZGTTOWXHYDDMFXRTADIMKMYBQACKHWUCAWADIRMXNQFPRJVYQJGHFESDAXOARREHNOXWRPRMUGAOIMMTCVHIRQPIQCZNRLLSIAMJNRZNKAWYCOCHEIWNSJRMQUVNNNHRIOCZZ')
             return true;
         else
@@ -86,13 +120,24 @@ class Morgan extends Controller
         $stdin = fopen(storage_path($root_files_path . 'input' . $filename . '.txt'), "r");
         fscanf($stdin, "%d\n", $t);
         $strings = [];
+        $test_this_only = 0;
         for ($t_itr = 0; $t_itr < $t; $t_itr++) {
             $a = '';
             fscanf($stdin, "%[^\n]", $a);
             $b = '';
             fscanf($stdin, "%[^\n]", $b);
-            $result = $this->morganAndString($a, $b);
-            $strings[] = $result;
+//             $a = 'JACK';
+             $a = 'ABACABA';
+//             $b = 'DANIEL';
+             $b = 'ABACABA';
+            if($test_this_only === $t_itr){
+                $result = $this->morganAndString($a, $b);
+                // var_dump($result === 'AABAABA', 'AABAABA');
+                // dump($result, $a, $b, $this->arr_string, $result);
+                // exit;
+                $strings[$test_this_only] = $result;
+                break;
+            }
         }
         fclose($stdin);
         $stdout = fopen(storage_path($root_files_path . 'output' . $filename . '.txt'), "r");
@@ -101,9 +146,12 @@ class Morgan extends Controller
             $expected[] = trim(preg_replace('/\s\s+/', '', fgets($stdout)));
         }
         fclose($stdout);
-        var_dump($strings[0]);
-        var_dump($expected[0]);
-        exit;
+//        dd($strings[$test_this_only] === 'DAJACKNIEL');
+        dd($strings[$test_this_only] === 'AABABACABACABA');
+        dd($strings[$test_this_only] === $expected[$test_this_only], count($expected));
+        dd(count($strings), count($expected), $expected === $strings);
+        dd($expected === $strings);
+        //exit;
         // dd(strlen($strings[0]), strlen($expected[0]), $strings[0] === $expected[0], $strings[0], $expected[0]);
         // dd(count($expected) === count($strings), $expected === $strings);
         //dd(count($expected) === count($strings));
--- app/Http/Controllers/MorganBackup.php
+++ app/Http/Controllers/MorganBackup.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 class MorganBackup extends Controller
 {
 
+    public function __construct()
+    {
+        $this->string = '';
+        $this->arr_string = [];
+    }
+
     /**
      * Handle the incoming request.
      *
@@ -18,107 +24,124 @@ class MorganBackup extends Controller
         $this->test($request);
     }
 
+    public function concat($string = '', $debug = false){
+        $this->arr_string[] = $string;
+        $this->string .= $string;
+        if($debug){
+            dd($string, $this->arr_string, $this->string);
+        }
+    }
+
     public function morganAndString($a, $b){
-//        $string1 = 'AABAB';
-//        $string2 = 'AABAA';
-//        $arr_string_1 = str_split($string1);
-//        $arr_string_2 = str_split($string2);
-//        $sum_string_1 = 0;
-//        $sum_string_2 = 0;
-//        foreach($arr_string_1 as $item){
-//            $sum_string_1 += ord($item);
-//        }
-//        foreach($arr_string_2 as $item){
-//            $sum_string_2 += ord($item);
-//        }
-//        dd($sum_string_1, $sum_string_2);
-        // dd($string1 < $string2, ord($string1) < ord($string2), ord($string1), ord($string2));
-        // dd(ord('A'),ord('Z'),ord('a'), ord('z'));
-        $string = '';
         $debug = false;
         while(isset($a[0]) && isset($b[0])){
+            $string1 = (string)$a;
+            $string2 = (string)$b;
             $ord_first_a = ord($a[0]);
             $ord_first_b = ord($b[0]);
 //            if($debug){
 //                dump('while start');
 //                dump($a[0] . ':' . $ord_first_a . '<>' . $b[0] . ':' . $ord_first_b);
-//                dump($a, $b, $string);
-//                dump(strlen($a), strlen($b), strlen($string));
 //                dd('while end');
 //            }
             if($ord_first_a < $ord_first_b){
-                $string .= $a[0];
-//                if($this->matched($string))
+                $character_taken = (string)$a[0];
+                $this->concat($a[0]);
+//                if($this->matched($this->string))
 //                    $debug = true;
                 $a = substr($a, 1);
+                $this->log($string1, $string2, $character_taken, $a, null, $this->string);
             }
             else if($ord_first_a > $ord_first_b){
-                $string .= $b[0];
-//                if($this->matched($string))
+                $character_taken = (string)$b[0];
+                $this->concat($b[0]);
+//                if($this->matched($this->string))
 //                    $debug = true;
                 $b = substr($b, 1);
-            }
-//            else if($ord_first_a === $ord_first_b && (!isset($a[1]) || !isset($b[1]))){
-//                $string .= $a[0];
-//                $a = substr($a, 1);
-//            }
-            else if($ord_first_a === $ord_first_b){
-                $string1 = $a;
-                $string2 = $b;
-                $arr_string_1 = str_split($string1);
-                $arr_string_2 = str_split($string2);
-                $sum_string_1 = 0;
-                $sum_string_2 = 0;
-                foreach($arr_string_1 as $item){
-                    $sum_string_1 += ord($item);
-                }
-                foreach($arr_string_2 as $item){
-                    $sum_string_2 += ord($item);
-                }
-                if($sum_string_1 < $sum_string_2){
-                    $string .= $a[0];
-                    $a = substr($a, 1);
-                }else if($sum_string_2 < $sum_string_1){
-                    $string .= $b[0];
-                    $b = substr($b, 1);
-                }
-                /*$longer_length = (strlen($a) >= strlen($b))?strlen($a):strlen($b);
+                $this->log($string1, $string2, $character_taken, null, $b, $this->string);
+            }else if($ord_first_a === $ord_first_b){
+                $longer_length = (strlen($a) >= strlen($b))?strlen($a):strlen($b);
                 for($counter = 0; $counter < $longer_length; $counter++){
-                    if(!isset($a[$counter]) || !isset($b[$counter]))
-                        break;
+                    if(!isset($a[$counter]) || !isset($b[$counter])){
+                        if($a == '' || $b == ''){
+                            break;
+                        }else{
+                            if(!isset($a[$counter])){
+                                $character_taken = (string)$a[0];
+                                $this->concat($a[0]);
+                                $a = substr($a, 1);
+                                $this->log($string1, $string2, $character_taken, $a, null, $this->string);
+                                break;
+                            }else if(!isset($b[$counter])){
+                                $character_taken = (string)$b[0];
+                                $this->concat($b[0]);
+                                $b = substr($b, 1);
+                                $this->log($string1, $string2, $character_taken, null, $b, $this->string);
+                                break;
+                            }else{
+                                dd(isset($a[$counter]), isset($b[$counter]), $a, $b, $this->string, 'one string end without any result, still issues');
+                            }
+                        }
+                    }
                     if(ord($a[$counter]) === ord($b[$counter])){
                         continue;
                     }else if(ord($a[$counter]) < ord($b[$counter])){
-                        $string .= $a[0];
+                        $character_taken = (string)$a[0];
+                        $this->concat($a[0]);
                         $a = substr($a, 1);
+                        $this->log($string1, $string2, $character_taken, $a, null, $this->string);
                         break;
                     }else if(ord($a[$counter]) > ord($b[$counter])){
-                        $string .= $b[0];
+                        $character_taken = (string)$b[0];
+                        $this->concat($b[0]);
                         $b = substr($b, 1);
+                        $this->log($string1, $string2, $character_taken, null, $b, $this->string);
                         break;
                     }
                 }
-                exit;
-                dump($a[0] . ' and ' . $b[0] . ' are equal out.', $a, $b);*/
             }
         }
-        $debug2 = false;
-        $remaining_string = (($a !== '' && $b === '')?$a:(($b !== '' && $a === '')?$b:''));
-        $remaining_string_count = strlen($remaining_string);
+        /*$debug2 = false;*/
+        $remaining_string = (($a !== '' && $b === '')?$a:(($b !== '' && $a === '')?$b:'*bigissue*'));
+        $this->concat($remaining_string);
+        /*$remaining_string_count = strlen($remaining_string);
         for($i=0; $i < $remaining_string_count; $i++){
-//            if($debug2){
-//                dump('remaining_string');
-//                dd($remaining_string);
-//            }
-            $string .= $remaining_string[0];
-//            if($this->matched($string))
+            if($debug2){
+                dump('remaining_string');
+                dd($remaining_string);
+            }
+            $this->concat($remaining_string[0]);
+//            if($this->matched($this->string))
 //                $debug2 = true;
             $remaining_string = substr($remaining_string, 1);
-        }
-        return $string;
+        }*/
+        return $this->string;
     }
 
+    public function log($string1 = null, $string2 = null, $take_char = null, $modified_string1 = null, $modified_string2 = null, $final_string = ''){
+        return false;
+        dump('=== Logging start === ');
+        dump('string 1 before : ' . $string1);
+        dump('string 2 before : ' . $string2);
+        dump('take char : ' . $take_char . ' from ' . (($modified_string1 !== null)? 'string 1':'string 2'));
+        if($modified_string1 !== null)
+            dump('string 1 after : ' . $modified_string1);
+        if($modified_string2 !== null)
+            dump('string 2 after : ' . $modified_string2);
+        if($final_string !== '')
+            dump('final string becomes : ' . $final_string);
+        dump('=== Logging end === ');
+    }
+
+    /*
+        public function update($string_num = 0, $concat = '', &$substr){
+            $this->concat($concat);
+            $substr = &substr($substr, 1);
+        }
+    */
+
     public function matched($str = null){
+        return false;
         if($str === 'KMNCCCGLIQXPRCEWDGRXNXZAZKMJEATBKNOVATQJJXCMFDQUJKQWVJZDNOYSXYTRDPPHJBCBMCRVMCAKRKLCQYKAUEVQQKAUQEAUEWPDQGHNFBHPXFXAGFMWUXFONMVGVQRMZFWTSIMBGHSGDLCROMQXWKMJSYJQRYVLEUAIHLCQTYHXWYMVLAOKPZPLKUBMYKINQFFOOXAFNYYGJBQKMWCSUTNKTJUVOJRPVYNPPLPPRVVKIVGWJTMQGHWYMTDIUGRKHEULXMGAVVVJJOGKIWRFYGKXOTHXCIYGTTLMMZQIATWDPEKKFTQGQHRRRWIAUPSOVTHYTHLXSXTDJILFXFNYCCLBTSHZHEHOVQFWNYGSNHLBDRRBXWQTECGPPKIIWHGQLILSJWRUUDMTAMNCKTMRFPBBRVXHXPXZUHSPHFDGYYISOJBIQCHZRUICMEHOLKJPEEDXORUXFYFEWCUCMDPOEPADIJNYGLLHEYKBWQRTDLXONRVOLGHCGOZXLAVCQBXZPZLBCEOSSXIRWWAJVYOPZGUSLHKAFFJUNOXVAEJXEMWKVIPMQNDHYDVVQQTWMXPUPNRRXFTFRTNZFESTQBJCKOSIKMXJAFJVXUMZOEJHLKORMLFXLSKNMYMJKSKBVJRDHPVFCLDWYICYXQBZYCAIOQRCGMKTEKDAHBNBILZCUOYSJETBTESGGSUWQWJSMFJORCLRKYRPRWJBKABHUYODSUAITIBDCAQIFSWJUMHQRWNBTRTESXHUIJVNQXQAVUDWIUOFEHKIVGYMKLDBEBZYMEEFFGBEHDVLFGDYSKOSABLCTRVSTMSGAWCTKPKAAMWMXSEIKZBRTVRBQORSFMWHSCMAFRJUGCMNZMLBYLNBCAWOFQPALTPETUCVAMYCPAEBRTKQHZUFEKIYYYHGSEMQCDCMCXNUVRNBLHYVKVASWZQQRRGLFRPZJOWKPQYEILDBWHSIBMCSNYQUCSSEMMJJDITAPMSRPNJMLSYULSJAWXIHZWODBVUDEKOEHGBNLCHFZXGXAZWZCWOCDJVPFSUHASCXVYTNMPWSWBWHGOZDIFSXKSNVBFUOCBDQUIIXBXBGLEHBGIKZTCAXNFLLYZVWCXVDOJLJCBWOKLCLEFGOKQEFMYCWPCLLTUWNSPNDKGEWEQFRDZGOAVGUWLARKGKFLIIPICSVJAPJNDXMZAGWSVWGFABDHIPSUHHVXADPIMJYUAQWQWKPDOIDKGANHZYUOCOWMNMZNHZWQWAHYPPXGGWUKXGLXVFVBZZRZEXZVMYRMNXFWAFDDSZIFZXTPLAPKQODXJIAPHPGPIXUFHXSAFJHEAMEJRKLFFRNYRSAVTRQXCGIAPRJKFDYIPIREXAEAFRKKSOQUIVWSADJBXAXSZBTTOFPAWHMLGSEWUBKOSFYSPUHNZUFWBJYKRWPBAEOUYHMTXOBVCSXFPQTUFCCDYTECZJPQCGIVJGHHOOLAKTCXFCJNFEACQYLNFIQJQAPTNZWXYSFTMOYHUZCUODUFWGRTUJRVDUMDDNMKODMTJFMVOJPFNLXXNPLBSIFZGYJUBGRCKGOUVASOADPNNRUUXEZMFRPNPFOUFZSDJWKGMZLIXFHRKAIRSKIMLSWKFJGMOCEAGFBYLCWELWFZIAZOMOUBNXJVNMDOUBYIFKHXWFMBRDWHRRMQXNHGUSQEMLLWUQJLSXSBNVIHQDIYTTAYEKFFXOOJYOUZGTTOWXHYDDMFXRTADIMKMYBQACKHWUCAWADIRMXNQFPRJVYQJGHFESDAXOARREHNOXWRPRMUGAOIMMTCVHIRQPIQCZNRLLSIAMJNRZNKAWYCOCHEIWNSJRMQUVNNNHRIOCZZ')
             return true;
         else
@@ -131,16 +154,21 @@ class MorganBackup extends Controller
         $stdin = fopen(storage_path($root_files_path . 'input' . $filename . '.txt'), "r");
         fscanf($stdin, "%d\n", $t);
         $strings = [];
+        $test_this_only = 1;
         for ($t_itr = 0; $t_itr < $t; $t_itr++) {
             $a = '';
             fscanf($stdin, "%[^\n]", $a);
             $b = '';
             fscanf($stdin, "%[^\n]", $b);
-            $a = 'AABAABA'; //'YZZYZYYZZYYZYZZY';
-            $b = 'AABABAA'; //'ZZYZYYZZYYZYZZY';
-            $result = $this->morganAndString($a, $b);
-            $strings[] = $result;
-            break;
+            // $a = 'AABAABA';
+            // $b = 'AABABAA';
+            if($test_this_only === $t_itr){
+                $result = $this->morganAndString($a, $b);
+                // var_dump($result === 'AABAABA', 'AABAABA');
+                // dump($result, $a, $b, $this->arr_string, $result);
+                // exit;
+                $strings[$test_this_only] = $result;
+            }
         }
         fclose($stdin);
         $stdout = fopen(storage_path($root_files_path . 'output' . $filename . '.txt'), "r");
@@ -149,17 +177,10 @@ class MorganBackup extends Controller
             $expected[] = trim(preg_replace('/\s\s+/', '', fgets($stdout)));
         }
         fclose($stdout);
-        var_dump($strings[0] === 'AABAABA', $strings[0]);exit;
-        /**
-        My WRONG output = YZZYZYYZZYYZYZZYZZYZYYZZYYZYZZY
-
-        My RIGHT output = YZZYZYYZZYYZYZZYZYYZZYYZYZZYZZY
-         */
-        var_dump(strlen($strings[0]), strlen($expected[0]));
-        var_dump($strings[0] == $expected[0]);
-        var_dump($strings[0]);
-        var_dump($expected[0]);
-        exit;
+        dd($strings[$test_this_only] === $expected[$test_this_only], count($expected));
+        dd(count($strings), count($expected), $expected === $strings);
+        dd($expected === $strings);
+        //exit;
         // dd(strlen($strings[0]), strlen($expected[0]), $strings[0] === $expected[0], $strings[0], $expected[0]);
         // dd(count($expected) === count($strings), $expected === $strings);
         //dd(count($expected) === count($strings));
